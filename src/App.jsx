ce0833c53621770f56049659b6bd24fb4a2ea28e
Anton <chiga2030@gmail.com>
2022-01-25 10:52:24 +0300

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
 import AppNavBar from './AppNavBar/AppNavBar';
 import LoginPage from './LoginPage/LoginPage';
 
-const App = () => (
-  <div className="d-flex flex-column vh-100">
-    <AppNavBar />
 
-    <LoginPage />
-  </div>
+const App = () => (
+  <Router>
+    <div className="d-flex flex-column vh-100">
+      <AppNavBar />
+
+      <Switch>
+        <Route path='/' exact>
+          <LoginPage />
+        </Route>
+
+        <Route path='/login'>
+          <LoginPage />
+        </Route>
+
+        <Redirect to='/' />
+      </Switch>
+    </div>
+  </Router>
 );
 
 
--- src/AppNavBar/AppNavBar.jsx
+++ src/AppNavBar/AppNavBar.jsx
@@ -1,3 +1,7 @@
+import {
+  NavLink,
+} from 'react-router-dom';
+
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -9,12 +13,22 @@ const AppNavBar = () => (
       <Navbar.Brand>
         Takeoff Staff
       </Navbar.Brand>
+
       <Nav className="me-auto flex-row">
-        <Nav.Link className="ms-2" href="login">Login</Nav.Link>
-        <Nav.Link className="ms-2" href="contacts">Contacts</Nav.Link>
+        <NavLink to="/login" className="ms-2">
+          Login
+        </NavLink>
+
+        <NavLink to="/contacnts" className="ms-2">
+          Contacts
+        </NavLink>
       </Nav>
+
       <Navbar.Text>
-        Signed in as: <a href="login">Guest</a>
+        Signed in as:
+        <NavLink to="/login" className="ms-1">
+          Guest
+        </NavLink>
       </Navbar.Text>
     </Navbar>
   </Container>
